dc361e721c38c72988ddb0465e1c6dfa3c0bdba5
Marcel <marcel.llopis@hotmail.com>
2019-07-12 15:32:55 +0200

    Feat: Afegim els sistema de test al main amb el seu respectiu menú

--- main.c
+++ main.c
@@ -2,8 +2,47 @@
 #include "llistaBID.h"
 
 int main() {
+
+    int opcio;
+    int num;
     LlistaBID l = LLISTABID_crea();
 
+    do{
+        printf("1. Introduir numero\n");
+        printf("2. Consultar pdi\n");
+        printf("3. Avançar pdi\n");
+        printf("4. Retrocedir pdi\n");
+        printf("5. Mostrar tota la llista\n");
+        printf("6. Sortir\n");
+        scanf("\nOpcio: %d", &opcio);
+    }while (opcio !=6);
+
+    switch opcio{
+        case 1:
+            printf("\nNumero major a 0: ");
+        scanf("%d", &num);
+        printf("\n");
+        LLISTABID_insereixOrdenat(&l, num);
+
+        case 2:
+            num = LLISTABID_consulta(l);
+        printf("%d\n", num);
+
+        case 3:
+            LLISTABID_avanca(&l);
+
+        case 4
+            LLISTABID_retrocedeix(&l);
+
+        case 5
+            LLISTABIO_vesInici(&l);
+        while(!LLISTABIO_fi(l)){
+            num = LLISTABID_consulta(l);
+            printf("%d\n", num);
+            LLISTABID_avanca(&l);
+        }
+    }
+    
     LLISTABID_destrueix(&l);
     return 0;
 }
